bc088cb936d1fe926bf66e6315dfd2b7aa05e6ba
megawall <prateekmahajan3@gmail.com>
2021-05-09 18:53:41 +0530

    posting data1

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET','POST'])
 def landingPage():
-    return jsonify({'name':"Prateek Mahajan"})
+    # return jsonify({'name':"Prateek Mahajan"})
+    data = request.get_json()
+    return data
 
 
 if __name__=='__main__':
